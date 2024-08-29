9d351ce808a2ef48a19a4f5b7463e4a4e3c8aab3
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-08-29 17:11:25 -0400

    docs: remove host from swagger spec
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- docs/docs.go
+++ docs/docs.go
@@ -470,7 +470,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost",
+	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{"http"},
 	Title:            "cardano-node-api",
