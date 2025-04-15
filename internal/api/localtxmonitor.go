82695aadf9fe982db692aed9f45ce01a19e0ed07
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-04-15 13:59:22 -0400

    chore(deps): adder/gouroboros/tx-submit-api updates for gouroboros 0.116.0 (#394)
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- internal/api/localtxmonitor.go
+++ internal/api/localtxmonitor.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Blink Labs Software
+// Copyright 2025 Blink Labs Software
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -205,7 +205,7 @@ func handleLocalTxMonitorTxs(c *gin.Context) {
 		resp = append(
 			resp,
 			responseLocalTxMonitorTxs{
-				TxHash:  tx.Hash(),
+				TxHash:  tx.Hash().String(),
 				TxBytes: txRawBytes,
 			},
 		)
