f81dddcba7d81fb304ebd6f1b2d13db794972bca
Chris Gianelloni <wolf31o2@blinklabs.io>
2026-01-25 15:04:14 -0500

    fix(openapi): update script for darwin and regen
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- openapi.sh
+++ openapi.sh
@@ -2,4 +2,4 @@
 
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i /local/docs/swagger.yaml --git-user-id blinklabs-io --git-repo-id cardano-node-api -g go -o /local/openapi -c /local/openapi-config.yml
 make format golines
-cd openapi && go mod tidy
+cd openapi && if [[ "$OSTYPE" == "darwin"* ]]; then sed -i '' 's/go 1.23/go 1.24.0/' go.mod; else sed -i 's/go 1.23/go 1.24.0/' go.mod; fi && go mod tidy
